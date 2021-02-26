337824def3ff9adc17c81a7324df098884d3de88
Evgeni Golov <evgeni@golov.de>
2021-02-26 13:15:51 +0100

    use jsvc

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 if [ -n "$1" ]; then
   exec "$@"
 else
-  exec /usr/bin/java -Xmx1024M -jar /usr/lib/unifi/lib/ace.jar start
+  # exec /usr/bin/java -Xmx1024M -jar /usr/lib/unifi/lib/ace.jar start
+  exec /bin/jsvc -cwd /usr/lib/unifi -home /usr/lib/jvm/java-8-openjdk-amd64 -cp /usr/share/java/commons-daemon.jar:/usr/lib/unifi/lib/ace.jar -pidfile /var/run/unifi/unifi.pid -procname unifi -outfile SYSLOG -errfile SYSLOG -umask 027 -user unifi -Dunifi.datadir=/var/lib/unifi -Dunifi.logdir=/var/log/unifi -Dunifi.rundir=/var/run/unifi -Dunifi.core.enabled=false -Xmx1024M -Djava.awt.headless=true -Dfile.encoding=UTF-8 -nodetach com.ubnt.ace.Launcher start
 fi
